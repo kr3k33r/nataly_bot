600321c82585d39a87bfc94bbfe58d91d5397ad2
kr3k33r <vaniash84@icloud.com>
2026-03-11 01:13:37 +0700

    init project

---
+++ nataly/src/main/java/ru/kreker/natalybot/NatalyApplication.java
@@ -0,0 +1,13 @@
+package ru.kreker.natalybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatalyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatalyApplication.class, args);
+    }
+
+}
